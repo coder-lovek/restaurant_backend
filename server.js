3edaea18cfdc0e5359cb51e7bbdcc7d5dea775d5
Love Kumar <142591319+coder-lovek@users.noreply.github.com>
2025-03-20 18:46:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import app from './app.js';
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.port,()=>{
+app.listen(PORT,()=>{
     console.log(`Server is  running very fast on port ${process.env.port}`);
 })
